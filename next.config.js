4d3212275b256d8ba0e8a94a4f60ef0cb2cff27c
Mike <mickydof@gmail.com>
2025-04-08 13:37:23 -0400

    Configure for GitHub Pages deployment

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/Type-Coder',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig 
